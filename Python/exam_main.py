7f5e5b02028f435b0193bf24fb07335d3c8decd2
OriginCode <origincoder@yahoo.com>
2018-11-24 02:28:53 +0800

    Python flavor (main) added.

---
+++ Python/exam_main.py
@@ -0,0 +1,63 @@
+import MODULE_0
+import MODULE_1
+
+# Global Variables
+GLOBAL_VAR_INT = 0
+GLOBAL_VAR_STR = "0"
+GLOBAL_VAR_LIST = [
+        0,
+        '0',
+        true
+]
+GLOBAL_VAR_TUPLE = (
+        0,
+        '0',
+        true
+)
+GLOBAL_VAR_DICT = {
+        'A': 0,
+        'B': '0',
+        'C': true
+}
+
+
+# Functions (With 2 Empty Lines Above)
+def function_0(x):
+    '''
+    comment
+    '''
+    do_something(
+            x,
+            '0',
+            true
+    )
+
+# if, while & for
+if not sth and sth > sth_else or sth <= sth_else: # not > and > or
+    do_something(
+            x,
+            '0',
+            true
+    )
+
+while sth != sth_else:
+    do_something(
+            x,
+            '0',
+            true
+    )
+
+for x in list:
+    do_something(
+            x,
+            '0',
+            true
+    )
+
+
+#TODO: xxx
+def main():
+    do_something()
+
+if __name__ = "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,5 @@
+# OriginCode's Code Flavor (Standard)
+
+## Supported Lang:
+
+	Python 2/3, Java (Incomplete/Learning)
